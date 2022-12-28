4e5d1d941631e79c62c33c72b82198c799ed894f
goar5670 <mahadelr19@gmail.com>
2022-12-28 23:37:03 +0300

    Setup initial argument parsing using clap

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
-use pikabit::{client::Client, constants};
+use clap::Parser;
+use pikabit::client::Client;
+
+#[derive(Parser)]
+struct Args {
+    filename: String,
+}
 
 #[tokio::main]
 async fn main() {
     log4rs::init_file("log_config.yml", Default::default()).unwrap();
 
-    const TORRENT_FILENAME: &str = constants::torrents::FREE_BSD;
-
-    let client = Client::new(TORRENT_FILENAME, None);
+    let args = Args::parse();
+    let client = Client::new(&args.filename, None);
 
     client.run().await;
 }
